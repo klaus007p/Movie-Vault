8bf66e2acee74464f3ef2eafa64a5edd1cdec970
Prabhat Pathak <klaus007p@gmail.com>
2026-08-03 23:30:01 +0530

    Added Account Creation Logic of appwrite users can create their accounts

--- src/appwrite/Authentication.js
+++ src/appwrite/Authentication.js
@@ -4,12 +4,35 @@ import { Client, Account, Id } from 'appwrite';
 export class AuthServive{
 
     client = new Client();
-    account = new Account();
+    account;
 
     constructor() {
         this.client
             .setEndpoint(conf.appwriteUrl) // your API EndPoint 
             .setProject(conf.appwriteProjectId) // Your Project Id
+
+        this.account = new Account(this.client)
     }
+
+    // Account Creation with Appwrite
+
+    async createAccount({ email, password, userName}) {
+        try {
+
+            const userAccount = await this.account.create
+            (ID.unique(), email, password, userName);
+            
+            if(userAccount) {
+                return this.login({ email, password})
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.log("Invalid Account Details: ", error);
+            // throw error
+        }
+    }
+
+    // Login In Account
 
 }
